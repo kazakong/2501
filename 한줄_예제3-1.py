d8f650e2de3fba9c79d284cd957519887f43bd20
khk <kazakong@naver.com>
2025-01-03 18:25:13 +0900

    2501

---
+++ 한줄_예제3-1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = np.array([1,2,3])
+
+print(a)
+
+b = np.array([[1,2],
+             [3,4]])
+
+print(b)
+
+c = np.array([[[1,2],[3,4]],
+             [[5,6],[7,8]]])
+
+print(c)
